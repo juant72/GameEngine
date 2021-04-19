ac1a6c7441e85b56cc03a0443b1952d13fb30b2a
Juan Tapia <juant72@gmail.com>
2021-04-18 22:31:14 -0300

    Add test project

---
+++ PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class PlayerCharacter: INotifyPropertyChanged
+    {
+        private int _health = 100;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Nickname { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
